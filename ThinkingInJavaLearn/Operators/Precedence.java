71379d257d24b4d0e119bfb5c954e010a364ae87
Zean Huang <1791293418@qq.com>
2017-09-22 21:42:23 +0800

    上传更新

--- ThinkingInJavaLearn/Operators/Precedence.java
+++
@@ -1,9 +0,0 @@
-class Precedence{ 
-    public static void main(String[] args){ 
-       //main program here... 
-       int x=1,y=2,z=3;
-       int a=x+y-2/2+z;
-       int b=x+(y-2)/(2+z);
-       System.out.println("a:"+a+" b:"+b);
-    }
-}
